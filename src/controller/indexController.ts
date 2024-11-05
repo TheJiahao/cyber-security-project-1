72dc622d64dceed1051975cd0bfc725465c085e9
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-11-05 17:10:54 +0200

    refactor: Move index.ejs to layouts

--- src/controller/indexController.ts
+++ src/controller/indexController.ts
@@ -2,6 +2,6 @@ import { FastifyInstance } from "fastify";
 
 export default function indexController(fastify: FastifyInstance) {
     fastify.get("/", (_, reply) => {
-        return reply.viewAsync("static/index.ejs");
+        return reply.viewAsync("layouts/index.ejs");
     });
 }

1f441a81b669bd760a9a48f5498a682ddb1a49e1
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-11-05 15:41:44 +0200

    style: Format

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
-import fastify from "fastify";
-import router from "router";
 import fastifyView from "@fastify/view";
 import ejs from "ejs";
+import fastify from "fastify";
+import router from "router";
 
 const server = fastify({
-  // Logger only for production
-  logger: !!(process.env.NODE_ENV !== "development"),
+    // Logger only for production
+    logger: !!(process.env.NODE_ENV !== "development"),
 });
 
 // Middleware: Router
 server.register(fastifyView, {
-  engine: {
-    ejs,
-  },
+    engine: {
+        ejs,
+    },
 });
 server.register(router);
 

e235977b1ef14973fc2ad9e11d10fe9bd4634808
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-11-05 18:00:24 +0200

    refactor: Merge homeRouter to router

--- src/routes/homeRouter.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyInstance } from "fastify";
-import { getView } from "utils/getView";
-
-export default function indexController(fastify: FastifyInstance) {
-    fastify.get("/", (_, reply) => {
-        return reply.viewAsync(getView("index.ejs"));
-    });
-}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import { FastifyInstance } from "fastify";
-import indexController from "routes/homeRouter";
 import userController from "routes/userRouter";
+import { getView } from "utils/getView";
 import { loginRouter } from "./loginRouter";
 
 export const router = (fastify: FastifyInstance) => {
+    fastify.get("/", (_, reply) => {
+        return reply.viewAsync(getView("index.ejs"));
+    });
+
     fastify.register(userController, { prefix: "/user" });
-    fastify.register(indexController, { prefix: "/" });
     fastify.register(loginRouter, { prefix: "/login" });
 };

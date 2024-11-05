89c0b25b546cb0ee70b7d549f7fd3c5952a02669
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-11-05 14:59:54 +0200

    ci: Configure prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+
+export default {
+  tabWidth: 4,
+  plugins: ["prettier-plugin-organize-imports"],
+  overrides: [
+    {
+      files: "*.yml",
+      options: {
+        tabWidth: 2,
+      },
+    },
+  ],
+};

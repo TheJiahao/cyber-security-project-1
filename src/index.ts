5d32627d590cac0295e9325884152f3ff5685901
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-11-05 15:48:16 +0200

    refactor: Remove unnecessary comments

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import app from "server";
+import server from "server";
 
 const FASTIFY_PORT = Number(process.env.FASTIFY_PORT) || 3006;
 
-app.listen({ port: FASTIFY_PORT });
+server.listen({ port: FASTIFY_PORT });
 
 console.log(
     `🚀  Fastify server running on port http://localhost:${FASTIFY_PORT}`,
--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,9 @@ import fastify from "fastify";
 import router from "router";
 
 const server = fastify({
-    // Logger only for production
     logger: !!(process.env.NODE_ENV !== "development"),
 });
 
-// Middleware: Router
 server.register(fastifyView, {
     engine: {
         ejs,

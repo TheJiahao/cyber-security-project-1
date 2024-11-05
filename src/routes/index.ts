93624fe54c0503d02edc3766b9de3cf66d02292d
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-11-05 17:54:29 +0200

    refactor: Rename user route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,6 @@ import indexController from "routes/homeRouter";
 import userController from "routes/userRouter";
 
 export const router = (fastify: FastifyInstance) => {
-    fastify.register(userController, { prefix: "/api/v1/user" });
+    fastify.register(userController, { prefix: "/user" });
     fastify.register(indexController, { prefix: "/" });
 };

439dcc3a29d5057c9ede9148ae5cf88197247b87
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-11-05 15:04:22 +0200

    ci: Configure ESLint

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import eslint from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  {
+    ignores: ["dist", "*.config.{js,ts,mjs}", "coverage"],
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+);

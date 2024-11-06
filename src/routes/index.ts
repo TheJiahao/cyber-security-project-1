1f054d7e9adfa266099aa9ff859206cffa9d6119
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-11-06 21:55:03 +0200

    feat: Show logged user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,25 @@
 import { FastifyInstance } from "fastify";
 import { userRouter } from "routes/userRouter";
+import { database } from "utils/database";
 import { getView } from "utils/getView";
 import { loginRouter } from "./loginRouter";
 import { registerRouter } from "./registerRouter";
 
 export const router = (fastify: FastifyInstance) => {
-    fastify.get("/", (_, reply) => {
-        return reply.viewAsync(getView("index.ejs"));
+    fastify.get("/", async (request, reply) => {
+        let user = null;
+
+        if (request.cookies.token) {
+            const { username } = await request.jwtVerify<{
+                username: string;
+            }>();
+
+            user = await database.user.findUnique({
+                where: { username },
+            });
+        }
+
+        return reply.viewAsync(getView("index.ejs"), { user });
     });
 
     fastify.register(userRouter, { prefix: "/user" });

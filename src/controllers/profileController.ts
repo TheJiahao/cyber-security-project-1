fec763f3fb607414fb199d3d2313d3d2e0a69dfe
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-11-09 20:50:58 +0200

    feat: Add route to update profile description

--- src/controllers/profileController.ts
+++ src/controllers/profileController.ts
@@ -6,6 +6,10 @@ interface ParamsType {
     username: string;
 }
 
+interface BodyType {
+    description: string;
+}
+
 export const profileController: FastifyPluginCallback = (fastify) => {
     fastify.get(
         "/:username",
@@ -17,6 +21,26 @@ export const profileController: FastifyPluginCallback = (fastify) => {
             });
 
             return reply.viewAsync(getView("profilePage.ejs"), { user });
+        },
+    );
+
+    fastify.post(
+        "/:username",
+        async (
+            request: FastifyRequest<{ Params: ParamsType; Body: BodyType }>,
+            reply,
+        ) => {
+            const { username } = request.params;
+            const { description } = request.body;
+
+            await database.user.update({
+                where: { username },
+                data: {
+                    description,
+                },
+            });
+
+            reply.redirect("/:username");
         },
     );
 };

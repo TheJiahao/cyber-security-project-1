f6fd7f9f89eebcedc94639eb6da01a09c4ff488d
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-11-09 20:39:34 +0200

    feat: Add profile page

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,7 +1,25 @@
-import { FastifyPluginCallback } from "fastify";
+import { FastifyPluginCallback, FastifyRequest } from "fastify";
+import { database } from "utils/database";
+import { getView } from "utils/getView";
 
 export const userRouter: FastifyPluginCallback = (fastify) => {
     fastify.get("/", function (_, reply) {
         reply.send({ message: "User" });
     });
+
+    fastify.get(
+        "/:username",
+        async (
+            request: FastifyRequest<{ Params: { username: string } }>,
+            reply,
+        ) => {
+            const { username } = request.params;
+
+            const user = await database.user.findUnique({
+                where: { username },
+            });
+
+            return reply.viewAsync(getView("profilePage.ejs"), { user });
+        },
+    );
 };

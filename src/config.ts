c10d91b0b2a281692e01e429ada311f129351e46
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-11-10 11:17:54 +0200

    docs: Add solution for weak secret key

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,12 @@
 export const VIEW_DIRECTORY = "src/views";
 export const SECRET_KEY = "1234";
+
+/*
+const SECRET_KEY = process.env["SECRET"];
+
+if (SECRET_KEY) {
+    console.error("Secret key missing");
+}
+
+export { SECRET_KEY };
+*/

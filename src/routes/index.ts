ab8312f0aea7f5d2b43dfe58db2f91348dcd8e38
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-11-10 11:06:40 +0200

    feat(home): List all users

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { userRouter } from "routes/userRouter";
+import { database } from "utils/database";
 import { getUser } from "utils/getUser";
 import { getView } from "utils/getView";
 import { authenticationRouter } from "./authenticationRouter";
@@ -7,8 +8,9 @@ import { authenticationRouter } from "./authenticationRouter";
 export const router = (fastify: FastifyInstance) => {
     fastify.get("/", async (request, reply) => {
         const user = await getUser(request);
+        const users = await database.user.findMany();
 
-        return reply.viewAsync(getView("index.ejs"), { user });
+        return reply.viewAsync(getView("index.ejs"), { user, users });
     });
 
     fastify.register(userRouter, { prefix: "/user" });
